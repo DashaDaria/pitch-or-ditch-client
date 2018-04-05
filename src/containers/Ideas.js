14e2ef0f04442c734ffacd4d4ca38d5172f0e9f6
DashaDaria <dashadariallc@gmail.com>
2018-04-05 17:16:29 -0600

    added styling to display each idea card

--- src/containers/Ideas.js
+++ src/containers/Ideas.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './IdeaCard.css';
 
 const Ideas = ({ideas}) => (
   <div>
     <h3>Ideas:</h3>
     {ideas.map(idea =>
-    <div>
+    <div className="IdeaCard" key={idea.id}>
       <h4>{idea.name} || <span>{idea.category_id}</span></h4>
       <p>{idea.content}</p>
       <small>Submitted by: @{idea.author}</small>
