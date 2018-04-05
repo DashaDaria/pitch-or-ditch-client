18b7b3dd9e46d500b43840a754a111bc86a406db
DashaDaria <dashadariallc@gmail.com>
2018-04-05 17:11:51 -0600

    added more information on each idea

--- src/containers/Ideas.js
+++ src/containers/Ideas.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Ideas = ({ideas}) => (
   <div>
-  <h3>Ideas:</h3>
-  {ideas.map(idea => <p>{idea.name}</p>)}
+    <h3>Ideas:</h3>
+    {ideas.map(idea =>
+    <div>
+      <h4>{idea.name} || <span>{idea.category_id}</span></h4>
+      <p>{idea.content}</p>
+      <small>Submitted by: @{idea.author}</small>
+    </div>)}
   </div>
 );
 
