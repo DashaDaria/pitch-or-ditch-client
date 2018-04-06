c9646176054cbef949edb85d90bb5fb4d6f02d61
DashaDaria <dashadariallc@gmail.com>
2018-04-06 11:44:12 -0600

    updated IdeaCard to reflect a presentational component

--- src/components/IdeaCard.js
+++ src/components/IdeaCard.js
@@ -1,13 +1,11 @@
+import React from 'react';
+
 const Ideas = ({ideas}) => (
-  <div>
-    <h3>Ideas:</h3>
-    {ideas.map(idea =>
     <div className="IdeaCard" key={idea.id}>
       <h4>{idea.name} || <span>{idea.category_name}</span></h4>
       <em className="Votes">Votes: {idea.votes}</em>
       <p>{idea.content}</p>
       <small>Submitted by: @{idea.author}</small>
-    </div>)}
   </div>
 );
 
