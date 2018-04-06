c50241907c493ab711bf155dfa1ebd7cac357ba0
DashaDaria <dashadariallc@gmail.com>
2018-04-05 21:24:19 -0600

    adjusted idea card display

--- src/containers/Ideas.js
+++ src/containers/Ideas.js
@@ -6,7 +6,8 @@ const Ideas = ({ideas}) => (
     <h3>Ideas:</h3>
     {ideas.map(idea =>
     <div className="IdeaCard" key={idea.id}>
-      <h4>{idea.name} || <span>{idea.category_id}</span></h4>
+      <h4>{idea.name} || <span>{idea.category_name}</span></h4>
+      <em className="Votes">Votes: {idea.votes}</em>
       <p>{idea.content}</p>
       <small>Submitted by: @{idea.author}</small>
     </div>)}
