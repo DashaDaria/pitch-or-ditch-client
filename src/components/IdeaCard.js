5a2e35768281394071394e941c8d5e338ea7397c
DashaDaria <dashadariallc@gmail.com>
2018-04-06 11:42:03 -0600

    connecting Ideas to the redux library via connect and mapping state via mapStateToProps

---
+++ src/components/IdeaCard.js
@@ -0,0 +1,14 @@
+const Ideas = ({ideas}) => (
+  <div>
+    <h3>Ideas:</h3>
+    {ideas.map(idea =>
+    <div className="IdeaCard" key={idea.id}>
+      <h4>{idea.name} || <span>{idea.category_name}</span></h4>
+      <em className="Votes">Votes: {idea.votes}</em>
+      <p>{idea.content}</p>
+      <small>Submitted by: @{idea.author}</small>
+    </div>)}
+  </div>
+);
+
+export default Ideas;
--- src/containers/Ideas.js
+++ src/containers/Ideas.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import IdeasCard from '../components/IdeaCard';
+import IdeaForm from './IdeaForm';
+import { getIdeas } from '../actions/ideas';
 import './IdeaCard.css';
 
 class Ideas extends Component {
@@ -13,25 +16,16 @@ class Ideas extends Component {
     return (
       <div className="IdeaCard">
         <h1>Ideas</h1>
-        {this.props.ideas.map(idea =>
-        <IdeaCard key={idea.id} idea={idea}/>
-      )}
+        {this.props.ideas.map(idea => <IdeaCard key={idea.id} idea={idea}/>)}
       </div>
     );
   }
 }
 
-const Ideas = ({ideas}) => (
-  <div>
-    <h3>Ideas:</h3>
-    {ideas.map(idea =>
-    <div className="IdeaCard" key={idea.id}>
-      <h4>{idea.name} || <span>{idea.category_name}</span></h4>
-      <em className="Votes">Votes: {idea.votes}</em>
-      <p>{idea.content}</p>
-      <small>Submitted by: @{idea.author}</small>
-    </div>)}
-  </div>
-);
+const mapStateToProps = (state) => {
+  return ({
+    ideas: state.ideas
+  })
+}
 
-export default Ideas;
+export default connect(mapStateToProps, { getIdeas })(Ideas);
