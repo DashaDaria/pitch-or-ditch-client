7d8668480bb0ea898f0d5a829156f91d48f43dce
DashaDaria <dashadariallc@gmail.com>
2018-04-06 21:01:23 -0600

    testing post request to api

--- src/actions/ideas.js
+++ src/actions/ideas.js
@@ -6,11 +6,27 @@ const setIdeas = ideas => {
   }
 }
 
+const addIdea = idea => {
+  return {
+    type: 'CREATE_IDEA_SUCCESS',
+    idea
+  }
+}
+
 export const getIdeas = () => {
   return dispatch => {
     return fetch('http://localhost:3001/api/ideas')
       .then(response => response.json())
       .then(ideas => dispatch(setIdeas(ideas)))
       .catch(error => console.log(error));
+  }
+}
+// 
+// export const createIdea = idea => {
+//   return dispatch => {
+//     return fetch('http://localhost:3001/api/ideas')
+//       .then(response => response.json())
+//       .then(ideas => dispatch(setIdeas(ideas)))
+//       .catch(error => console.log(error));
   }
 }
