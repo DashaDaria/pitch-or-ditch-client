51f8e98660a26d36c556a33c7519f6cd22783706
DashaDaria <dashadariallc@gmail.com>
2018-04-08 16:34:02 -0600

    workign with possible router urls

--- src/index.js
+++ src/index.js
@@ -8,12 +8,42 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js';
+import WelcomeHeader from './components/WelcomeHeader';
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <div>
-        <App />
+        
+        <Route path='/home' render={WelcomeHeader} />
+        <Route exact path='/about' render={About} />
+        <Route exact path='/login' render={Login} />
       </div>
     </Router>
   </Provider>,
