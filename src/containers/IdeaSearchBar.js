102d30b2794ce4ea885e68dc5a1d501bd5c00101
DashaDaria <dashadariallc@gmail.com>
2018-04-07 21:41:49 -0600

    select drop down for shoosing category

--- src/containers/IdeaSearchBar.js
+++ src/containers/IdeaSearchBar.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
 
 class IdeaSearchBar extends Component {
+
+  handleOnChange = event => {
+    const { name, value } = event.target;
+    console.log(name)
+
+  }
+
+
   render(){
     return (
       <div>
-      <label htmlFor="category">Search Ideas By Category:</label>
-        <select name="category_id" value={category_id} onChange={this.handleOnChange}>
-          <option></option>
-          <option value="1">Social</option>
-          <option value="2">Digital</option>
-          <option value="3">Partnerships</option>
-          <option value="4">On-Air</option>
-          <option value="5">On-Ground</option>
-        </select>
+      <form >
+        <div>
+        <label htmlFor="category">Search Ideas By Category:</label>
+          <select name="category_id" >
+            <option></option>
+            <option value="1">Social</option>
+            <option value="2">Digital</option>
+            <option value="3">Partnerships</option>
+            <option value="4">On-Air</option>
+            <option value="5">On-Ground</option>
+          </select>
+        </div>
+        <button type="submit">Filter</button>
+        </form>
       </div>
     )
   }
 }
 
-export default IdeaSearchBar;
+
+ export default IdeaSearchBar;
