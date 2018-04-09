07e4d4b5bfbbfd52d98aef2094ba328a3fac97a8
DashaDaria <dashadariallc@gmail.com>
2018-04-09 12:23:47 -0600

    creating navbar and navlink as components

--- src/index.js
+++ src/index.js
@@ -10,64 +10,12 @@ import registerServiceWorker from './registerServiceWorker';
 import store from './store.js';
 import WelcomeHeader from './components/WelcomeHeader';
 
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
-  textDecoration: 'none',
-  color: 'white',
-}
-
-/* add the navbar component */
-const Navbar = () =>
-  <div>
-    <NavLink
-      to="/"
-      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-      exact
-      /* add styling to Navlink */
-      style={link}
-      /* add prop for activeStyle */
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Home</NavLink>
-    <NavLink
-      to="/about"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >About</NavLink>
-    <NavLink
-      to="/login"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Login</NavLink>
-  </div>;
+import Navbar from './components/Navbar';
 
 const Home = () => <h1>Home!</h1>;
 
 const About = () => <h1>This is my about component!</h1>;
 
-const Login = () =>
-  <form>
-    <h1>Login</h1>
-    <div>
-      <input type="text" name="username" placeholder="Username" />
-      <label htmlFor="username">Username</label>
-    </div>
-    <div>
-      <input type="password" name="password" placeholder="Password" />
-      <label htmlFor="password">Password</label>
-    </div>
-    <input type="submit" value="Login" />
-  </form>;
 
 ReactDOM.render(
   <Provider store={store}>
@@ -78,7 +26,6 @@ ReactDOM.render(
         <Route path='/'  component={App} />
         <Route exact path='/home' render={WelcomeHeader} />
         <Route exact path='/about' render={About} />
-        <Route exact path='/login' render={Login} />
       </div>
     </Router>
   </Provider>,
