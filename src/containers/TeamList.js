0072936feb02ca66ecddf38334681c112c254e23
DashaDaria <dashadariallc@gmail.com>
2018-04-12 14:53:25 -0600

    team list container component with table render

--- src/containers/TeamList.js
+++ src/containers/TeamList.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Table } from 'react-bootstrap';
 
 class TeamList extends Component {
+
+  renderTeam(idea){
+    return(
+      <tr key={idea.author}>
+        <td>{idea.author}</td>
+        <td>{idea.name}</td>
+      </tr>
+    )
+  }
+
   render(){
     return(
-      <div>
-        Hi
-      </div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Who</th>
+            <th>Wrote</th>
+          </tr>
+          </thead>
+          <tbody>
+              {this.props.ideas.map(this.renderTeam)}
+          </tbody>
+      </Table>
     )
   }
 }
 
-export default TeamList;
+function mapStateToProps({ideas}) {
+  return { ideas };
+}
+
+export default connect(mapStateToProps)(TeamList);
